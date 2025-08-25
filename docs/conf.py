7d3a7b111443f2f141d21d4872edab34e696d44c
Essi Parent <9ivko1ozf@relay.firefox.com>
2025-08-25 14:16:50 -0400

    unique workflow

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../src'))
 project = 'ggoat'
 copyright = '2024, ggoat development team'
 author = 'ggoat development team'
-release = '0.1.0'
+release = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
